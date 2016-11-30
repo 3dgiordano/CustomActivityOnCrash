eff287d49e61e4ee1ec0ecdeac0046b84365e6fc
David <3dgiordano@gmail.com>
2016-11-30 16:21:01 -0300

    Update buddybuild_postbuild.sh

--- buddybuild_postbuild.sh
+++ buddybuild_postbuild.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 curl -sL http://monkop.com/cli -o monkop-cli.py
-python monkop-cli.py -k $MONKOP_KEY -w -a $BUDDYBUILD_APKS/release-sample-release.apk
+python monkop-cli.py -k $MONKOP_KEY -a $BUDDYBUILD_APKS/release-sample-release.apk
